fedd5fb015390f80d15637c0c1acef6af980985b
BeanLeaf <gashumbaaimable@gmail.com>
2025-11-22 05:40:09 -0800

    fix(factory): ensure Faker instance in TrainingSessionRecordFactory

--- database/factories/TrainingSessionRecordFactory.php
+++ database/factories/TrainingSessionRecordFactory.php
@@ -14,23 +14,25 @@ class TrainingSessionRecordFactory extends Factory
     {
         $coach = User::inRandomOrder()->first();
 
+        $faker = $this->faker ?? \Faker\Factory::create();
+
         return [
             'coach_id' => $coach?->id,
             'coach_name' => $coach?->name,
-            'branch' => $this->faker->randomElement(['Rwanda','Tanzania']),
-            'training_pitch' => $this->faker->word(),
-            'date' => $this->faker->dateTimeBetween('-1 month', '+1 month')->format('Y-m-d'),
-            'start_time' => $this->faker->time('H:i:s'),
-            'finish_time' => $this->faker->time('H:i:s'),
-            'main_topic' => $this->faker->sentence(3),
-            'area_performance' => $this->faker->word(),
-            'part1_activities' => $this->faker->paragraph(),
-            'part2_activities' => $this->faker->paragraph(),
-            'part3_notes' => $this->faker->paragraph(),
-            'part4_message' => $this->faker->sentence(),
-            'number_of_kids' => $this->faker->numberBetween(0,50),
-            'incident_report' => $this->faker->optional()->sentence(),
-            'missed_damaged_equipment' => $this->faker->optional()->sentence(),
+            'branch' => $faker->randomElement(['Rwanda','Tanzania']),
+            'training_pitch' => $faker->word(),
+            'date' => $faker->dateTimeBetween('-1 month', '+1 month')->format('Y-m-d'),
+            'start_time' => $faker->time('H:i:s'),
+            'finish_time' => $faker->time('H:i:s'),
+            'main_topic' => $faker->sentence(3),
+            'area_performance' => $faker->word(),
+            'part1_activities' => $faker->paragraph(),
+            'part2_activities' => $faker->paragraph(),
+            'part3_notes' => $faker->paragraph(),
+            'part4_message' => $faker->sentence(),
+            'number_of_kids' => $faker->numberBetween(0,50),
+            'incident_report' => $faker->optional()->sentence(),
+            'missed_damaged_equipment' => $faker->optional()->sentence(),
         ];
     }
 }
