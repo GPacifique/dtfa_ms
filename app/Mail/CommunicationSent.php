0cdba561c255aeceebe7d66367f69dbd60c70de9
GPacifique <GPacifique@users.noreply.github.com>
2026-02-13 06:38:20 -1000

    Fix communications email sending - use send() instead of queue() in job and remove ShouldQueue from mailable

--- app/Mail/CommunicationSent.php
+++ app/Mail/CommunicationSent.php
@@ -5,12 +5,11 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use App\Models\Communication;
 
-class CommunicationSent extends Mailable implements ShouldQueue
+class CommunicationSent extends Mailable
 {
-    use Queueable, SerializesModels;
+    use SerializesModels;
 
     public $communication;
 
